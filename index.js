82d3d406e1fba49a4ec2712bb21bd75bd9254ed6
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2024-03-03 11:39:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// const express = require('express')
+// const app = express()
+// require('dotenv').config()
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get("/rakshith",(req,res) =>{
+//     res.send("Learning Backend!!!!!")
+// })
+
+
+// app.get('/login',(req,res)=>{
+//     res.send("<h1>PLease learn here by logging in.</h1>")
+// })
+
+
+// app.get('/nandini',(req,res)=>{
+//     res.send("<h2>I love Chai Very Much</h2>")
+// })
+
+// app.listen(port = process.env.PORT, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
